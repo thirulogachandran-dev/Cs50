d7d4525136d1473a81f724961379dfdbf5b94fda
thirulogachandran-dev <289444359+thirulogachandran-dev@users.noreply.github.com>
2026-06-01 10:03:32 +0530

    added greet.c

---
+++ greet/greet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char name[25];
+	int age;
+	printf("Your name : ");
+	scanf("%s",name);
+	printf("Your age : ");
+	scanf("%d",&age);
+	printf("Hello %s ,you are %d years old.\n",name ,age);
+	return 0;
+}
